300236a064a2b3cee95cffef2a4b802eea579e84
Ellen Shapiro <designatednerd@gmail.com>
2023-02-26 17:47:59 -0600

    Add tests to component lib for colors

--- Packages/ComponentLibrary/Tests/ComponentLibraryTests/ComponentLibraryTests.swift
+++ Packages/ComponentLibrary/Tests/ComponentLibraryTests/ComponentLibraryTests.swift
@@ -1,5 +1,16 @@
 import XCTest
+import SwiftUI
 @testable import ComponentLibrary
 
 final class ComponentLibraryTests: XCTestCase {
+    
+    func testColorsAllPullValidValueFromAssetCatalog() {
+        let invalidColor = Color("Definitely not legit", bundle: .module)
+        XCTAssertNil(invalidColor.cgColor, "Not legit color should not have a CGColor")
+        
+        for color in AssetCatalogColor.allCases {
+            XCTAssertNil(color.color.cgColor,
+                         "Color \(color.rawValue) has a nil CGColor, indicating it was not pulled out of the asset catalog!")
+        }
+    }
 }
